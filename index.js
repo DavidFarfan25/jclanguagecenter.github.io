d14812aae5b68c26a14a5277e537ca02f1010454
DavidFarfan25 <137664540+DavidFarfan25@users.noreply.github.com>
2024-10-06 23:48:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".tabla1", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal(".header", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "-100px",
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal(".container", {
+  duration: 4000,
+  origin: "left",
+  distance: "400px",
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal(".niveles", {
+  duration: 4000,
+  origin: "right",
+  distance: "400px",
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+  duration: 2000,
+  origin: "left",
+  distance: "400px",
+  easing: "ease-in-out",
+});
